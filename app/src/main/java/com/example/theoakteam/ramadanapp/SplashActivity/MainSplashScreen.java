2d679615abbe4e4dad564cc480b30e698db6ca8f
Mahadi Hasan Sunny <sunny_mhs@hotmail.com>
2015-05-30 23:37:05 +0600

    I add Splash Activity

---
+++ app/src/main/java/com/example/theoakteam/ramadanapp/SplashActivity/MainSplashScreen.java
@@ -0,0 +1,52 @@
+package com.example.theoakteam.ramadanapp.SplashActivity;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+
+import com.example.theoakteam.ramadanapp.R;
+import com.example.theoakteam.ramadanapp.SehriAndIfterShortForm;
+
+
+public class MainSplashScreen extends Activity {
+//hello it  is test for github
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main_splash_screen);
+
+
+
+        Thread background = new Thread() {
+            public void run() {
+
+                try {
+
+                    sleep(2000);
+
+
+                    Intent i=new Intent(MainSplashScreen.this,SehriAndIfterShortForm.class);
+                    startActivity(i);
+
+
+                    finish();
+
+                } catch (Exception e) {
+
+                }
+            }
+        };
+
+        // start thread
+        background.start();
+
+
+    }
+
+    @Override
+    protected void onDestroy() {
+
+        super.onDestroy();
+
+    }
+}
