8fee1b0524cd343a9203ab150b1c4acaaff8b2da
Mahadi Hasan Sunny <sunny_mhs@hotmail.com>
2015-06-14 20:54:54 +0600

    I removed some settings function

--- app/src/main/java/theoaktroop/appoframadan/SettingActivity/SettingsActivity.java
+++ app/src/main/java/theoaktroop/appoframadan/SettingActivity/SettingsActivity.java
@@ -104,56 +104,56 @@ public class SettingsActivity
 //        tasbihInitialization();
         districtInitialization();
 
-        if(sharedPreferences.contains("time"))
-        {  preTime=sharedPreferences.getString("time",checkShareprefernce);
-            pickTime.setText(preTime);
-
-        }
-        else {
-            pickTime.setText(getString(R.string.txt_time));
-
-
-        }
-        if(sharedPreferences.contains("checkbox"))
-        {
-            checkBoxcheck=sharedPreferences.getInt("checkbox",0);
-            if(checkBoxcheck==0)
-            {
-                callChechkBoxMakeGone();
-            }
-            else if(checkBoxcheck==1){
-                callChechkBoxMakeVisibile();
-            }
-        }
-        allCheckh();
-
-
-        checkBoxUser.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if (checkBoxUser.isChecked() == false) {
-                    callChechkBoxMakeGone();
-                }
-                if (checkBoxUser.isChecked() == true) {
-                    callChechkBoxMakeVisibile();
-
-                }
-            }
-        });
-//hkjk
-
-        pickTime.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                timeString = new StringBuilder();
-                final Calendar cal = Calendar.getInstance();
-                pHour = cal.get(Calendar.HOUR_OF_DAY);
-                pMinute = cal.get(Calendar.MINUTE);
-                showDialog(TIME_DIALOG_ID);
-                makeSureButtonCheckhed = true;
-
-
-            }
-        });
+//        if(sharedPreferences.contains("time"))
+//        {  preTime=sharedPreferences.getString("time",checkShareprefernce);
+//            pickTime.setText(preTime);
+//
+//        }
+//        else {
+//            pickTime.setText(getString(R.string.txt_time));
+//
+//
+//        }
+//        if(sharedPreferences.contains("checkbox"))
+//        {
+//            checkBoxcheck=sharedPreferences.getInt("checkbox",0);
+//            if(checkBoxcheck==0)
+//            {
+//                callChechkBoxMakeGone();
+//            }
+//            else if(checkBoxcheck==1){
+//                callChechkBoxMakeVisibile();
+//            }
+//        }
+//        allCheckh();
+//
+//
+//        checkBoxUser.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                if (checkBoxUser.isChecked() == false) {
+//                    callChechkBoxMakeGone();
+//                }
+//                if (checkBoxUser.isChecked() == true) {
+//                    callChechkBoxMakeVisibile();
+//
+//                }
+//            }
+//        });
+////hkjk
+//
+//        pickTime.setOnClickListener(new View.OnClickListener() {
+//            public void onClick(View v) {
+//                timeString = new StringBuilder();
+//                final Calendar cal = Calendar.getInstance();
+//                pHour = cal.get(Calendar.HOUR_OF_DAY);
+//                pMinute = cal.get(Calendar.MINUTE);
+//                showDialog(TIME_DIALOG_ID);
+//                makeSureButtonCheckhed = true;
+//
+//
+//            }
+//        });
 
 
         //below code use for drawer
@@ -269,29 +269,29 @@ public class SettingsActivity
         }
     }
 
-
-
-
-
-    private void callChechkBoxMakeGone() {
-        checkBoxUser.setChecked(false);
-        editor.putInt("checkbox", 0);
-        userTimeLayout.setVisibility(View.GONE);
-
-    }
-
-    private void callChechkBoxMakeVisibile() {
-        checkBoxUser.setChecked(true);
-        editor.putInt("checkbox", 1);
-        if(preTime!=null && preTime.length()!=0)
-        {
-            pickTime.setText(preTime);
-        }
-        else {
-            pickTime.setText(getResources().getString(R.string.txt_time));
-        }
-        userTimeLayout.setVisibility(View.VISIBLE);
-    }
+//
+//
+//
+//
+//    private void callChechkBoxMakeGone() {
+//        checkBoxUser.setChecked(false);
+//        editor.putInt("checkbox", 0);
+//        userTimeLayout.setVisibility(View.GONE);
+//
+//    }
+//
+//    private void callChechkBoxMakeVisibile() {
+//        checkBoxUser.setChecked(true);
+//        editor.putInt("checkbox", 1);
+//        if(preTime!=null && preTime.length()!=0)
+//        {
+//            pickTime.setText(preTime);
+//        }
+//        else {
+//            pickTime.setText(getResources().getString(R.string.txt_time));
+//        }
+//        userTimeLayout.setVisibility(View.VISIBLE);
+//    }
 
     //Hasan's Code area start
 //    public void tasbihInitialization() {
@@ -374,9 +374,9 @@ public class SettingsActivity
 
 
     private void intilizationOfViews() {
-        pickTime = (Button) findViewById(R.id.bttimeshow);
-        checkBoxUser = (CheckBox) findViewById(R.id.checkBoxmain);
-        userTimeLayout=(LinearLayout)findViewById(R.id.notification_user_time);
+       // pickTime = (Button) findViewById(R.id.bttimeshow);
+       // checkBoxUser = (CheckBox) findViewById(R.id.checkBoxmain);
+      //  userTimeLayout=(LinearLayout)findViewById(R.id.notification_user_time);
         timeString  =new StringBuilder();
         sharedPreferences = getSharedPreferences("RamadanAppData", Context.MODE_PRIVATE);
         editor = sharedPreferences.edit();
@@ -401,118 +401,118 @@ public class SettingsActivity
 
 
     }
+//
+//    private void timeCalculation()
+//    {
+//        //for background thread
+//        final Calendar cal = Calendar.getInstance();
+//       int nowHour	= cal.get(Calendar.HOUR_OF_DAY);
+//       int nowMinute = cal.get(Calendar.MINUTE);
+//
+//        finalHour=pfinalHour-nowHour;
+//        finalMinute=pfinalMinute-nowMinute;
+//        if(finalMinute<0)
+//        {
+//            finalMinute+=60;
+//            finalHour-=1;
+//        }
+//        if(finalHour<0)
+//        {
+//            finalHour+=24;
+//        }
+//        System.out.println("Final Minute=" + finalMinute);
+//        System.out.println("Final Hour=" + finalHour);
+//        finalMinute+=finalHour*60;
+//        System.out.println("Final Minute aftercalculation=" + finalMinute);
+//        System.out.println("Final Milliseconds aftercalculation=" + finalMinute * 60 * 1000);
+//
+//
+//    }
 
-    private void timeCalculation()
-    {
-        //for background thread
-        final Calendar cal = Calendar.getInstance();
-       int nowHour	= cal.get(Calendar.HOUR_OF_DAY);
-       int nowMinute = cal.get(Calendar.MINUTE);
-
-        finalHour=pfinalHour-nowHour;
-        finalMinute=pfinalMinute-nowMinute;
-        if(finalMinute<0)
-        {
-            finalMinute+=60;
-            finalHour-=1;
-        }
-        if(finalHour<0)
-        {
-            finalHour+=24;
-        }
-        System.out.println("Final Minute=" + finalMinute);
-        System.out.println("Final Hour=" + finalHour);
-        finalMinute+=finalHour*60;
-        System.out.println("Final Minute aftercalculation=" + finalMinute);
-        System.out.println("Final Milliseconds aftercalculation=" + finalMinute * 60 * 1000);
-
-
-    }
-
-void allCheckh(){
-    if(makeSureButtonCheckhed==true || checkBoxUser.isChecked() == true) {
-        callChechkBoxMakeVisibile();
-        timeCalculation();
-        editor.putString("time", timeString.toString());
-        editor.putLong("finaltime",finalMinute);
-        editor.commit();
-        if (finalMinute!=0)
-        {Intent serviceIntent=new Intent(getApplicationContext(),NotifyingService.class);
-
-            startService(serviceIntent);}
-
-
-
-        makeSureButtonCheckhed=false;
-    }
-    else {
-        editor.commit();
-
-        Intent serviceIntent=new Intent(getApplicationContext(),NotifyingService.class);
-
-        stopService(serviceIntent);
-    }
-}
-    @Override
-    protected void onPause() {
-        super.onPause();
-allCheckh();
-
-        finish();
-
-    }
-
-
-    private TimePickerDialog.OnTimeSetListener mTimeSetListener =
-            new TimePickerDialog.OnTimeSetListener() {
-                public void onTimeSet(TimePicker view, int hourOfDay, int minute ) {
-                    pfinalHour=hourOfDay;
-                    pfinalMinute=minute;
-                    pMinute = minute;
-                    if(hourOfDay<12) {
-                        AMPM = " AM";
-                        pHour = hourOfDay;
-                    }
-                    else { pHour = hourOfDay-12;
-                        AMPM=" PM";
-                    }
-                    if(pHour==0){
-                        pHour=12;
-                    }
-                    updateDisplay();
-
-
-                }
-            };
-    @Override
-    protected Dialog onCreateDialog(int id) {
-        switch (id) {
-            case TIME_DIALOG_ID:
-                return new TimePickerDialog(this,
-                        mTimeSetListener, pHour, pMinute+1, false);
-        }
-        return null;
-    }
-
-    /** Add padding to numbers less than ten */
-    private static String pad(int c) {
-        if (c >= 10)
-            return String.valueOf(c);
-        else
-            return "0" + String.valueOf(c);
-    }
-
-    private void updateDisplay() {
-
-        timeString.append(pad(pHour)).append(":");
-        timeString.append(pad(pMinute));
-        timeString.append(AMPM);
-        if(pickTime.getContext().toString().length()>0) {
-            pickTime.setText(timeString.toString());
-        }
-        timeCalculation();
-
-    }
+//void allCheckh(){
+//    if(makeSureButtonCheckhed==true || checkBoxUser.isChecked() == true) {
+//        callChechkBoxMakeVisibile();
+//        timeCalculation();
+//        editor.putString("time", timeString.toString());
+//        editor.putLong("finaltime",finalMinute);
+//        editor.commit();
+//        if (finalMinute!=0)
+//        {Intent serviceIntent=new Intent(getApplicationContext(),NotifyingService.class);
+//
+//            startService(serviceIntent);}
+//
+//
+//
+//        makeSureButtonCheckhed=false;
+//    }
+//    else {
+//        editor.commit();
+//
+//        Intent serviceIntent=new Intent(getApplicationContext(),NotifyingService.class);
+//
+//        stopService(serviceIntent);
+//    }
+//}
+//    @Override
+//    protected void onPause() {
+//        super.onPause();
+//allCheckh();
+//
+//        finish();
+//
+//    }
+//
+//
+//    private TimePickerDialog.OnTimeSetListener mTimeSetListener =
+//            new TimePickerDialog.OnTimeSetListener() {
+//                public void onTimeSet(TimePicker view, int hourOfDay, int minute ) {
+//                    pfinalHour=hourOfDay;
+//                    pfinalMinute=minute;
+//                    pMinute = minute;
+//                    if(hourOfDay<12) {
+//                        AMPM = " AM";
+//                        pHour = hourOfDay;
+//                    }
+//                    else { pHour = hourOfDay-12;
+//                        AMPM=" PM";
+//                    }
+//                    if(pHour==0){
+//                        pHour=12;
+//                    }
+//                    updateDisplay();
+//
+//
+//                }
+//            };
+//    @Override
+//    protected Dialog onCreateDialog(int id) {
+//        switch (id) {
+//            case TIME_DIALOG_ID:
+//                return new TimePickerDialog(this,
+//                        mTimeSetListener, pHour, pMinute+1, false);
+//        }
+//        return null;
+//    }
+//
+//    /** Add padding to numbers less than ten */
+//    private static String pad(int c) {
+//        if (c >= 10)
+//            return String.valueOf(c);
+//        else
+//            return "0" + String.valueOf(c);
+//    }
+//
+//    private void updateDisplay() {
+//
+//        timeString.append(pad(pHour)).append(":");
+//        timeString.append(pad(pMinute));
+//        timeString.append(AMPM);
+//        if(pickTime.getContext().toString().length()>0) {
+//            pickTime.setText(timeString.toString());
+//        }
+//        timeCalculation();
+//
+//    }
 
 
 
