05612d8c23ec610cb51d99e4fb1faa1ee7910c3c
Mahadi Hasan Sunny <sunny_mhs@hotmail.com>
2015-06-18 11:19:57 +0600

    Fragment Navigation drwer and remove sHeriTltle  from drawer

--- app/src/main/java/theoaktroop/appoframadan/NavigationDrawerActivity/NavigationDrawerFragment.java
+++ app/src/main/java/theoaktroop/appoframadan/NavigationDrawerActivity/NavigationDrawerFragment.java
@@ -309,8 +309,7 @@ public class NavigationDrawerFragment extends Fragment {
 
     @Override
     public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
-        // If the drawer is open, show the global app actions in the action bar. See also
-        // showGlobalContextActionBar, which controls the top-left area of the action bar.
+
         if (mDrawerLayout != null && isDrawerOpen()) {
             inflater.inflate(R.menu.global, menu);
             showGlobalContextActionBar();
